4945a768b340c31b6d5104260d99ecd9fc2a9f9e
Cameron Graybill <csg58@drexel.edu>
2019-06-03 20:15:05 -0400

    Adding graph stuff

--- parse_data_file.py
+++ parse_data_file.py
@@ -1,77 +1,55 @@
-from itertools import count
+from itertools import count, chain
 from json import loads
 from sys import argv
 
+import cv2
 import matplotlib.pyplot as plt
+import networkx as nx
 import numpy as np
 
 unique_val = count(0)
 
-import networkx as nx
-import io
-import cv2
-from itertools import chain
 
-def makevid():
+def make_peer_graph(path_to_input):
+    vid = cv2.VideoWriter(f'./test.avi', 0, 2, (640, 480))
+    data = get_data(path_to_input)
     G = nx.Graph()
-    vid = cv2.VideoWriter(f'./test.avi', 0, 2.5, (640, 480))
+    target_clients = sorted(list(set(x['id'] for x in data['data'])))[:5]
+    all_peers = set(chain.from_iterable(x['peers'] for x in data['data'] if x['id'] in target_clients))
+    all_clients = all_peers | set(target_clients)
+    G.add_nodes_from(all_clients)
+    pos = nx.circular_layout(all_clients)
 
-    with open('./results/DropZeros/50_80_20_4_100_100.json') as f:
-        data = loads(f.read())
-        id = 0
+    for iteration in range(get_end(data)):
+        entries = [x for x in data['data'] if
+                   x['iteration'] == iteration and (x['id'] in all_peers or x['id'] in target_clients)]
+        good_entries = [x for x in entries if not x['free_rider']]
+        bad_entries = [x for x in entries if x['free_rider']]
+        clients_at_this_iter = (x for x in entries if x['id'] in target_clients)
 
-        gud = []
-        bad = []
-        edges = []
+        def edges():
+            for client in clients_at_this_iter:
+                for peer in client['peers']:
+                    yield (client['id'], peer)
 
-        for i in range(data['metadata']['iterations']):
-            idx = i * (data['metadata']['starting_good_clients'] + data['metadata']['starting_bad_clients'])
-            agent = data['data'][idx]
-            print(agent)
-            edges.append([(agent['id'], data['data'][idx + p]['id']) for p in agent['peers']])
-            for p in agent['peers']:
-                peer_idx = idx + p
-                peer = data['data'][peer_idx]
-                if peer['free_rider']:
-                    bad.append(peer['id'])
-                else:
-                    gud.append(peer['id'])
+        G.add_edges_from(edges())
+        nx.draw_networkx_nodes(G, pos, nodelist=[x['id'] for x in good_entries], node_color='g', node_size=100,
+                               alpha=0.8)
+        nx.draw_networkx_nodes(G, pos, nodelist=[x['id'] for x in bad_entries], node_color='r', node_size=100,
+                               alpha=0.8)
+        nx.draw_networkx_edges(G, pos, alpha=0.5)
+        plt.xlim((-1.1, 1.1))
+        plt.ylim((-1.1, 1.1))
+        plt.axis("off")
+        plt.title(f"{data['metadata']['strategy']} Iteration {iteration}")
+        plt.savefig('./img.png', format='png')
+        img = cv2.imread('./img.png')
+        vid.write(img)
+        plt.clf()
+        G.remove_edges_from(list(G.edges))
+    cv2.destroyAllWindows()
+    vid.release()
 
-        print(len(gud))
-        print(gud)
-        print(len(bad))
-        print(bad)
-        #G.add_nodes_from(chain(gud, bad))
-
-
-        #nx.draw(G)
-        #plt.show()
-        for grp in edges:
-            plt.clf()
-            G = nx.Graph()
-            G.add_nodes_from(chain(data['data'][0], gud, bad))
-
-            #G.remove_edges_from([x for x in G.edges()])
-            print(grp)
-            G.add_edges_from(grp)
-
-            pos = nx.spring_layout(G)
-
-            nx.draw_networkx_nodes(G, pos, nodelist=gud, node_color='g', node_size=100, alpha=0.8)
-            nx.draw_networkx_nodes(G, pos, nodelist=bad, node_color='r', node_size=100, alpha=0.8)
-            nx.draw_networkx_edges(G, pos, alpha=0.5)
-
-            plt.savefig('./img.png', format='png')
-            img = cv2.imread('./img.png')
-
-            vid.write(img)
-
-        cv2.destroyAllWindows()
-        vid.release()
-
-
-if __name__ == '__main__':
-    makevid()
 
 def get_data(path_to_file):
     with open(path_to_file) as f:
@@ -179,39 +157,28 @@ def make_utility_graphs(path_to_file):
     f.show()
 
 
-
-def make_all_graphs(path_to_file):
+def make_all_graphs(path_to_file, vid=False):
     make_population_graphs(path_to_file)
     make_happiness_graphs(path_to_file)
     make_utility_graphs(path_to_file)
+    if vid:
+        make_peer_graph(path_to_file)
 
 
 if __name__ == '__main__':
-    makevid()
-    # try:
-    #     path = argv[2]
-    #     cmd = argv[1]
-    #     try:
-    #         {
-    #             'pop': make_population_graphs,
-    #             'happy': make_happiness_graphs,
-    #             'util': make_utility_graphs,
-    #             'all': make_all_graphs
-    #         }[cmd](path)
-    #         plt.show()
-    #     except KeyError:
-    #         print("valid commands are 'pop', 'happy', 'util', and 'all'")
-    # except:
-    #     print(f"usage: {argv[0]} pop|happy|util|all path/to/json/file")
-    #     good_guys = [
-    #         y for y in all_data['data'] if not y['free_rider']
-    #     ]
-
-    #     bad_guys = [
-    #         y for y in all_data['data'] if y['free_rider']
-    #     ]
-
-    # plt.plot([sum(a['iteration'] == x for a in good_guys) for x in range(all_data['metadata']['iterations'])], 'g')
-    # plt.plot([sum(a['iteration'] == x for a in bad_guys) for x in range(all_data['metadata']['iterations'])], 'r')
-    # plt.plot([sum(a['iteration'] == x for a in all_data['data']) for x in range(all_data['metadata']['iterations'])], 'b')
-    # plt.show()
+    try:
+        path = argv[2]
+        cmd = argv[1]
+        try:
+            {
+                'pop': make_population_graphs,
+                'happy': make_happiness_graphs,
+                'util': make_utility_graphs,
+                'all': make_all_graphs,
+                'vid': make_peer_graph
+            }[cmd](path)
+            plt.show()
+        except KeyError:
+            print("valid commands are 'pop', 'happy', 'util', 'vid', and 'all'")
+    except:
+        print(f"usage: {argv[0]} pop|happy|util|all|vid path/to/json/file")
